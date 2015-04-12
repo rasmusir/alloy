3ee6ae01e3618de2299470ab4318c9ec391a7dc6
Raspberry <israelssonrasmusraz@gmail.com>
2015-04-12 19:59:52 +0000

    New development branch
    Former-commit-id: ee0b10e96a30be72a12c0d180df49233f38fc3c5

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         </form>
         hah, these were wonky as heck
         <br>
-        <img src="http://cute-n-tiny.com/wp-content/uploads/2009/12/cute-panda-playing-400x300.jpg">
+        <img src="">
         Indeed it is.
         <?php
             echo phpinfo();
